e9d82541dfd1dfb8dde9cdc02312f8d84acf5a6d
nouish <1390631+nouish@users.noreply.github.com>
2025-09-06 04:23:35 +0200

    Consolidate shop GUIs
    The infinite stock "admin shops" remain separate for now. I want to consolidate these eventually, but this change already feels like enough risk for now.

--- core/src/main/java/me/deadlight/ezchestshop/listeners/ChestOpeningListener.java
+++ core/src/main/java/me/deadlight/ezchestshop/listeners/ChestOpeningListener.java
@@ -9,10 +9,8 @@ import me.deadlight.ezchestshop.EzChestShopConstants;
 import me.deadlight.ezchestshop.data.Config;
 import me.deadlight.ezchestshop.data.ShopCommandManager;
 import me.deadlight.ezchestshop.data.ShopContainer;
-import me.deadlight.ezchestshop.guis.AdminShopGUI;
-import me.deadlight.ezchestshop.guis.NonOwnerShopGUI;
-import me.deadlight.ezchestshop.guis.OwnerShopGUI;
 import me.deadlight.ezchestshop.guis.ServerShopGUI;
+import me.deadlight.ezchestshop.guis.ShopGUI;
 import me.deadlight.ezchestshop.integrations.CoreProtectIntegration;
 import me.deadlight.ezchestshop.utils.BlockOutline;
 import me.deadlight.ezchestshop.utils.Utils;
@@ -41,10 +39,6 @@ import org.slf4j.Logger;
 
 public class ChestOpeningListener implements Listener {
     private static final Logger LOGGER = EzChestShop.logger();
-
-    private final NonOwnerShopGUI nonOwnerShopGUI= new NonOwnerShopGUI();
-    private final OwnerShopGUI ownerShopGUI = new OwnerShopGUI();
-    private final AdminShopGUI adminShopGUI = new AdminShopGUI();
 
     // We don't ignore cancelled events because we may have to ignore
     // permission plugins that register on LOWEST priority anyway.
@@ -153,17 +147,7 @@ public class ChestOpeningListener implements Listener {
             }
             // At this point it is clear that some shop will open, so run opening commands here.
             Config.shopCommandManager.executeCommands(player, loc, ShopCommandManager.ShopType.SHOP, ShopCommandManager.ShopAction.OPEN, null);
-            if (player.hasPermission("ecs.admin") || player.hasPermission("ecs.admin.view")) {
-                adminShopGUI.showGUI(player, dataContainer, chestblock);
-                return;
-            }
-
-            if (player.getUniqueId().toString().equalsIgnoreCase(owneruuid) || isAdmin) {
-                ownerShopGUI.showGUI(player, dataContainer, chestblock, isAdmin);
-            } else {
-                //not owner show default
-                nonOwnerShopGUI.showGUI(player, dataContainer, chestblock);
-            }
+            ShopGUI.showGUI(player, dataContainer, chestblock, isAdmin);
         }
     }
 
